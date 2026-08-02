001c6f7e07ffa54e7eeb84da642aeec97963b4da
Mateusz <mbuzek.buzek@gmail.com>
2026-08-02 14:56:19 +0200

    refactor: restructure repository to include only project code at root and exclude external packages

--- gym-donkeycar-25.10.06/gym_donkeycar/navigation/__init__.py
+++
@@ -1,5 +0,0 @@
-from .lidar_slam import LidarSLAM
-from .grid_mapper import GridMapper
-from .global_planner import GlobalPlanner
-from .icp_optimizer import ICPOptimizer
-from .local_planner import LocalPlanner
--- gym-donkeycar-master/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Gymnasium Environments for Donkey Car in a project::
-
-    import gym_donkeycar
